1a826446b54a5e0bd59911ea07e3130b260a6646
indrajit <eendroroy@gmail.com>
2021-07-24 00:04:41 +0600

    update UnsupportedBaseValue exception

--- src/main/kotlin/com/github/eendroroy/kotp/exception/UnsupportedBaseValue.kt
+++ src/main/kotlin/com/github/eendroroy/kotp/exception/UnsupportedBaseValue.kt
@@ -5,4 +5,12 @@ import java.lang.RuntimeException
 /**
  * @author indrajit
  */
-class UnsupportedBaseValue: RuntimeException("supports only base value 10")
+class UnsupportedBaseValue : RuntimeException("supports only base value $PROV_BASE_VALUE") {
+    companion object {
+        const val PROV_BASE_VALUE = 10
+
+        fun passOrThrow(value: Int) {
+            if (PROV_BASE_VALUE != value) throw UnsupportedBaseValue()
+        }
+    }
+}
